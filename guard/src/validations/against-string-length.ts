8b10794634437b4e473c591542c318fdd9681ea3
Vinh Lam <vinh.lam@zoi.de>
2022-11-25 17:45:41 +0700

    testing

--- guard/src/validations/against-string-length.ts
+++ guard/src/validations/against-string-length.ts
@@ -2,8 +2,8 @@ import { GuardAgainstBasic } from '../bases';
 
 class AgainstStringLength extends GuardAgainstBasic {
   private readonly text: string;
-  private readonly maxLength: number;
-  private readonly minLength: number;
+  private readonly maxLength?: number;
+  private readonly minLength?: number;
 
   constructor({
     text,
@@ -11,23 +11,29 @@ class AgainstStringLength extends GuardAgainstBasic {
     minLength,
   }: {
     text: string;
-    maxLength: number;
-    minLength: number;
+    maxLength?: number;
+    minLength?: number;
   }) {
     super();
     this.text = text;
     this.maxLength = maxLength;
     this.minLength = minLength;
+
+    if (!this.minLength || !this.maxLength) {
+      throw new Error(
+        `[Constructor error]: Need at at least 1 parameter in minLength/maxLength`,
+      );
+    }
   }
 
   handle() {
-    if (this.text.length < this.minLength) {
+    if (this.minLength && this.text.length < this.minLength) {
       throw new Error(
         `[Validation error]: Text's minimum length is ${this.minLength}, current text's length: ${this.text.length}`,
       );
     }
 
-    if (this.text.length > this.maxLength) {
+    if (this.maxLength && this.text.length > this.maxLength) {
       throw new Error(
         `[Validation error]: Text's maximum length is ${this.maxLength}, current text's length: ${this.text.length}`,
       );
